3fe16cc6e997931b92dbf6a08390913874e46256
El Guill <71549232+Spidfail@users.noreply.github.com>
2022-01-13 19:15:02 +0100

    container : accessor / capacity and some
    + operator=();
    + assign();
    Everything tested

--- vector/ft_vector.hpp
+++ vector/ft_vector.hpp
@@ -6,7 +6,7 @@
 /*   By: guhernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 18:59:24 by guhernan          #+#    #+#             */
-/*   Updated: 2022/01/12 20:31:59 by guhernan         ###   ########.fr       */
+/*   Updated: 2022/01/13 18:36:05 by guhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,19 +56,19 @@ class FtVector {
 				_start(NULL), _last(NULL), _size(0), _capacity(0), _alloc(alloc) {
 		}
 
-		FtVector(const FtVector<value_type> &target) :
-				_start(NULL), _last(NULL), _size(0), _capacity(0) {
+		FtVector(const FtVector<value_type> &target, const allocator_type &alloc = allocator_type()) :
+				_start(NULL), _last(NULL), _size(0), _capacity(0), _alloc(alloc) {
 
 			*this = target;
 		}
 
-		FtVector(const size_type &n, const value_type &val = value_type(),
+		FtVector(size_type n, const value_type &val = value_type(),
 				const allocator_type &alloc = allocator_type()) :
 				_start(NULL), _last(NULL), _size(n), _capacity(n), _alloc(alloc) {
 
 			this->_start = this->_alloc.allocate(n);
-			for (size_type i = 0 ; i < n ; i++) {
-				this->_alloc.construct(this->_start + i, val);
+			while (--n) {
+				this->_alloc.construct(this->_start + n, val);
 			}
 			this->_last = this->start + n;
 		}
@@ -82,17 +82,36 @@ class FtVector {
 	//
 	//// OPERATORS
 	//
-		FtVector		&operator=(const FtVector<value_type> &) {
-
+		FtVector		&operator=(const FtVector<value_type> &source) {
+			pointer		temp = _reallocate(source.size());
+			_copy(source.data(), source.data() + source.size(), temp);
+			_erase_all();
+			_set_members(temp, source.size(), source.size());
 		}
 
 		reference		operator[](size_type pos) {
-			return this->_start + pos;
+			return &(this->_start + pos);
 		}
 
 		const_reference	operator[](size_type pos) const {
-			return this->_start + pos;
+			return &(this->_start + pos);
 		}
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	//
+	//////// MEMBERS FUNCTIONS
+	//
+		void	assign(size_type count, const value_type &value) {
+			pointer		temp = this->_alloc.allocate(count);
+			_erase_all();
+			for (int i = 0; i < count ; i++) {
+				temp[i] = value;
+			}
+			_set_members(temp, count, count);
+		}
+
+		// void	assign() {      \\ iterator version
+		// }
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	//
 	//// ACCESSORS : at(), front(), back(), data()
@@ -100,7 +119,7 @@ class FtVector {
 		reference		at(size_type pos) {
 			if (pos < 0 || pos > _capacity)
 				throw(std::out_of_range("Out of range"));
-			return &this->_start + pos;
+			return &(this->_start + pos);
 		}
 
 		const_reference	at(size_type pos) const {
@@ -132,7 +151,7 @@ class FtVector {
 		const_pointer	data() const {
 			return this->_start;
 		}
-	
+
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	//
 	//// CAPACITY : empty(), size(), max_size(), reserve(), capacity()
@@ -145,10 +164,21 @@ class FtVector {
 			return this->size;
 		}
 
-		// size_type	max_size() const {
-		// }
-
 		void		reserve(size_type new_cap) {
+			if (new_cap > _capacity) {
+					pointer	temp = _reallocate(new_cap);
+					_copy(_start, _last, temp);
+					_erase_all();
+					_set_members(temp, new_cap, new_cap);
+			}
+		}
+
+		size_type	max_size() const throw() {
+			return std::numeric_limits<size_type>::max() / sizeof(value_type);
+		}
+
+		size_type	capacity() const {
+			return this->_capacity;
 		}
 
 
@@ -156,11 +186,38 @@ class FtVector {
 
 		pointer			_start;
 		pointer			_last;
-
 		size_type		_size;
 		size_type		_capacity;
-
 		allocator_type	_alloc;
+
+		pointer			_reallocate(size_type n) const {
+			pointer		replace;
+			replace.allocate(n);
+			return replace;
+		}
+
+		void			_copy(const pointer start, const pointer end, pointer target) const {
+			for (int i = 0 ; start + i != end && i < target.capacity() ; i++)
+				target[i] = start[i];
+		}
+
+		void			_set_members(const pointer pointer, size_type size, size_type capacity) {
+			this->_start = pointer;
+			this->_last = pointer + (capacity - 1);
+			this->_size = size;
+			this->_capacity = capacity;
+		}
+
+		void			_erase_all() {
+			if (_start == NULL)
+				return ;
+			pointer		temp = _start;
+			while (temp != _last) {
+				_alloc.destroy(temp);
+				temp++;
+			}
+			_alloc.deallocate(_start, _capacity);
+		}
 
 };
 
--- vector/main.cpp
+++ vector/main.cpp
@@ -6,7 +6,7 @@
 /*   By: guhernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 19:21:21 by guhernan          #+#    #+#             */
-/*   Updated: 2022/01/12 19:37:06 by guhernan         ###   ########.fr       */
+/*   Updated: 2022/01/13 18:39:16 by guhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ void	print_state(std::vector<std::string> const &target) {
 
 int		main(int ac, char **av) {
 
+	(void)av;
+
 
 	// FtVector<std::string>	test;
 	// FtVector<std::string>	test2(test);
@@ -37,20 +39,68 @@ int		main(int ac, char **av) {
 	std::cout << " /////////////////////////////////////////////////// " << std::endl;
 
 
-	std::vector<std::string>		args(200, "Manger des frites");
+	std::vector<std::string>		args(200, "manger des frites");
+
+	args[199] = "manger des MOULES";
+	args[0] = "manger des BULOTS";
 
 	print_state(args);
 
-	for (int i = 1; av[i] ; i++) {
-		std::cout << " /////////////////////////////////////////////////// " << std::endl;
-		args.push_back(av[i]);
-		print_state(args);
+	std::vector<std::string>		testCopy(args);
+
+	print_state(testCopy);
+
+
+
+
+	std::cout << "//////////////// Test accessors " << std::endl;
+	std::cout << " //////////////////        out of range :" << std::endl;
+	try {
+		std::cout << "test at : [custom] = {" << testCopy.at(200) << "}	--	[official] {" << args.at(200) << "}" << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
 	}
 
-	////////////////////////////////////////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////////////////////
-	////////////////////////////////////////////////////////////////////////////////////////////
+	try {
+		std::cout << "test [] : [custom] = {" << testCopy[200] << "}	--	[official] {" << args[200] << "}" << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 
+	std::cout << " //////////////////        Valide :" << std::endl;
+	std::cout << "test at : [custom] = {" << testCopy.at(100) << "}	--	[official] {" << args.at(100) << "}" << std::endl;
+	std::cout << "test [] : [custom] = {" << testCopy[100] << "}	--	[official] {" << args[100] << "}" << std::endl;
+
+
+	std::cout << "test front : [custom] = {" << testCopy.front() << "}	--	[official] {" << args.front() << "}" << std::endl;
+	std::cout << "test back : [custom] = {" << testCopy.back() << "}	--	[official] {" << args.back() << "}" << std::endl;
+	std::cout << "test data : [custom] = {" << *testCopy.data() << "}	--	[official] {" << *args.data() << "}" << std::endl;
+
+
+
+
+	std::cout << "//////////////// Test capacity " << std::endl;
+	std::cout << " //////////////////        Exception :" << std::endl;
+
+
+	std::cout << " //////////////////        Valide :" << std::endl;
+
+	std::cout << "test empty : [custom] = {" << testCopy.empty() << "}	--	[official] {" << args.empty() << "}" << std::endl;
+	std::cout << "test size : [custom] = {" << testCopy.size() << "}	--	[official] {" << args.size() << "}" << std::endl;
+
+	args.reserve(300);
+	testCopy.reserve(300);
+	std::cout << "test reserve : [custom] = {" << testCopy.size() << "}	--	[official] {" << args.size() << "}" << std::endl;
+	std::cout << "test reserve : [custom] = {" << testCopy.capacity() << "}	--	[official] {" << args.capacity() << "}" << std::endl;
+
+	std::cout << "test max_size : [custom] = {" << testCopy.max_size() << "}	--	[official] {" << args.max_size() << "}" << std::endl;
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////
+//
 	// std::allocator< std::string >		alloc;
 //
 	// std::string		*testAlloc = alloc.allocate(5);
@@ -63,7 +113,9 @@ int		main(int ac, char **av) {
 	// for (int i = 0 ; !testAlloc[i].empty() ; i++) {
 		// std::cout << testAlloc[i] << std::endl;
 	// }
-
+//
+	// std::cout << (testAlloc + 3) - testAlloc << std::endl;
+//
 	////////////////////////////////////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////////////////////
